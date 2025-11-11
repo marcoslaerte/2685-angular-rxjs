77f907a1ba9103a21de2510b895159fefcd50cf4
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-11-11 17:08:58 -0300

    Adicionando o operador distinctUntilChanged que não permite efetuar a busca pelo mesmo termo duas vezes seguidas

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -23,6 +23,7 @@ export class ListaLivrosComponent {
       debounceTime(PAUSA), //Delay de espera após o usuário terminar de digitar para não efetuar busca antes dele completar a palavra de busca
       filter((valorDigitado) => valorDigitado.length >= 3), //Pesquisa somente após o usuário digitar pelo menos 3 caracteres
       tap(() => console.log('Fluxo Inicial')),
+      distinctUntilChanged(),
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)), //SwitchMap descarta os valores anteriores (os primeiros caracteres digitados) e envia somente o valor integral (ou seja, a palavra inteira).
       tap((retornoAPI) => console.log(retornoAPI)),
       map((items) => this.livrosResultadoParaLivros(items))
