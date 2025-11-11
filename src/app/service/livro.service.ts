c9d1f1eabc4ebc448fbf73487e3ee6a3209b0458
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-11-11 17:39:48 -0300

    Adicionando catchError e throwError para capturar erro na requisição

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -8,7 +8,7 @@ import { Item, LivrosResultado } from '../models/interfaces';
 })
 export class LivroService {
 
-  private readonly API = 'https://www.googleapis.com/books/v1/volumes'
+  private readonly API = 'https://www.googleapis.com/books/v1/volume'
 
   constructor(private http: HttpClient) { }
 
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -26,7 +26,11 @@ export class ListaLivrosComponent {
       distinctUntilChanged(),
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)), //SwitchMap descarta os valores anteriores (os primeiros caracteres digitados) e envia somente o valor integral (ou seja, a palavra inteira).
       tap((retornoAPI) => console.log(retornoAPI)),
-      map((items) => this.livrosResultadoParaLivros(items))
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError(erro => {
+        console.log(erro);
+        return throwError(() => new Error('Ops, ocorreu um erro!'));
+      })
     )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
