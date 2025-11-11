971d3b972ec3cfb4f8c19936ef6de5bc5436c776
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-11-11 17:00:16 -0300

    Adicionando debounceTime para efetuar a busca somente após o usuário digitar todos os caracteres

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,9 +1,11 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { filter, map, switchMap, tap } from 'rxjs';
+import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
@@ -18,10 +20,11 @@ export class ListaLivrosComponent {
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
-      filter((valorDigitado) => valorDigitado.length >= 3),
+      debounceTime(PAUSA), //Delay de espera após o usuário terminar de digitar para não efetuar busca antes dele completar a palavra de busca
+      filter((valorDigitado) => valorDigitado.length >= 3), //Pesquisa somente após o usuário digitar pelo menos 3 caracteres
       tap(() => console.log('Fluxo Inicial')),
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)), //SwitchMap descarta os valores anteriores (os primeiros caracteres digitados) e envia somente o valor integral (ou seja, a palavra inteira).
-      tap(() => console.log('Requisições ao servidor')),
+      tap((retornoAPI) => console.log(retornoAPI)),
       map((items) => this.livrosResultadoParaLivros(items))
     )
 
